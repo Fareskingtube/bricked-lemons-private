eb17cfe658ba4725660a1bb10776fe2293426cfe
Fareskingtube <83561978+Fareskingtube@users.noreply.github.com>
2026-07-10 03:20:57 +0300

    Add login page and axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,38 @@
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import axios from "axios";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+	const [user, setUser] = useState(null);
+	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const res = await axios.get("/auth/me");
+				setUser(res.data);
+			} catch (error) {
+				setUser(null);
+			} finally {
+				setLoading(false);
+			}
+		};
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Navbar />
+			<Toaster/>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
 			</Routes>
 		</BrowserRouter>
 	);
