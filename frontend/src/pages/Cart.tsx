6abc99cc1755cb301d4fcc9b5f37a311da6c9349
Fareskingtube <83561978+Fareskingtube@users.noreply.github.com>
2026-07-12 05:22:34 +0300

    Change Cart background

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -15,7 +15,8 @@ function Cart() {
 
 	return (
 		<div className="w-screen h-screen mt-10">
-			<div className="p-5 mx-10 bg-background-300 min-h-[80vh] flex flex-col gap-2 justify-between items-center rounded-2xl">
+			<div className="p-5 mx-10 bg-background-300 dark:bg-background-50 dark:border-2 dark:border-background-100 min-h-[80vh] flex flex-col gap-2 
+            justify-between items-center rounded-2xl">
 				<h1>Cart ({cart.length} Items)</h1>
 				<div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] w-full gap-4 items-center justify-center">
 					{cart.map((product) => (
