9e09bbd75e78bbb0d5edbecbb4abc7b36f5975b8
Fareskingtube <83561978+Fareskingtube@users.noreply.github.com>
2026-08-12 00:15:03 +0300

    Add working category dropdown in navbar (no longer work in progress)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import Cart from "./pages/Cart";
 import ProductItem from "./pages/ProductIDetails";
 import { UserProvider } from "./hooks/UseUser";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Profile from "./pages/Profile";
 import Admin from "./pages/Admin";
 import WorkInProgress from "./pages/WorkInProgress";
@@ -32,12 +32,11 @@ function App() {
 						<Route path="/cart" element={<Cart />} />
 						<Route path="/profile" element={<Profile />} />
 						<Route path="/admin" element={<Admin />} />
-						<Route path="/categories" element={<WorkInProgress />} />
 						<Route path="/deals" element={<WorkInProgress />} />
 						<Route path="/new" element={<WorkInProgress />} />
 					</Routes>
 					{/* TODO: Remove */}
-					<ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+					{/* <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" /> */}
 				</QueryClientProvider>
 			</BrowserRouter>
 		</UserProvider>
