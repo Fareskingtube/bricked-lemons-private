e48c02bcffe363bff264e81613f6948fc1d22b41
fareskingtube <fareskingtube@gmail.com>
2026-08-15 12:42:55 +0300

    Add try catch to Nodemailer NEVER AGAIN

--- backend/src/config/nodemailer.ts
+++ backend/src/config/nodemailer.ts
@@ -1,22 +1,29 @@
-import nodemailer from "nodemailer"
-import { requireEnv } from "./env.ts"
+import nodemailer from "nodemailer";
+import { requireEnv } from "./env.ts";
 
-const email = requireEnv("NODE_MAILER_EMAIL") 
-const password = requireEnv("NODE_MAILER_PASSWORD")
+const email = requireEnv("NODE_MAILER_EMAIL");
+const password = requireEnv("NODE_MAILER_PASSWORD");
 
 // Creating nodemailer transport
 export const transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: email,
-        pass: password
-    }
-})
+	service: "gmail",
+	auth: {
+		user: email,
+		pass: password,
+	},
+});
 
 // Checking if credentials are correct
-transport.verify((err) => {
-    if (err) {
-        console.error("[mailer] Failed to authenticate with SMTP server:", err.message)
-        process.exit(1)
-    }
-})
+try {
+	transport.verify((err) => {
+		if (err) {
+			console.error(
+				"[mailer] Failed to authenticate with SMTP server:",
+				err.message,
+			);
+			process.exit(1);
+		}
+	});
+} catch (error) {
+	console.log("Node mailer error: ", error);
+}
